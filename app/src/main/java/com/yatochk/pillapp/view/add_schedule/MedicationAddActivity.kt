312891420d18a86c55de655e9de5e0b1356f90ec
yatochk <progrusa@gmail.com>
2019-06-04 01:45:56 +0400

    add all medication items

--- app/src/main/java/com/yatochk/pillapp/view/add_schedule/MedicationAddActivity.kt
+++ app/src/main/java/com/yatochk/pillapp/view/add_schedule/MedicationAddActivity.kt
@@ -1,8 +1,10 @@
 package com.yatochk.pillapp.view.add_schedule
 
+import android.view.View
 import com.yatochk.pillapp.R
 import com.yatochk.pillapp.dagger.MedicationApplication
 import com.yatochk.pillapp.view.ToolActivity
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MedicationAddActivity : ToolActivity() {
 
@@ -12,5 +14,6 @@ class MedicationAddActivity : ToolActivity() {
     override fun initActivity() {
         setContentView(R.layout.activity_add_medication)
         (application as MedicationApplication).component.injectActivity(this)
+        button_tool_accept.visibility = View.INVISIBLE
     }
 }
