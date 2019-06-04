3976fc2bc86fb62f4ec74a3ca065396fb69702e7
yatochk <progrusa@gmail.com>
2019-06-04 23:27:04 +0400

    add values and converters

---
+++ app/src/main/java/com/yatochk/pillapp/model/MedicationEat.kt
@@ -0,0 +1,7 @@
+package com.yatochk.pillapp.model
+
+enum class MedicationEat {
+    BEFORE,
+    IN_TIME,
+    AFTER
+}
--- app/src/main/java/com/yatochk/pillapp/model/MedicationSchedule.kt
+++ app/src/main/java/com/yatochk/pillapp/model/MedicationSchedule.kt
@@ -4,6 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
 import com.yatochk.pillapp.model.db.ListTypeConverter
+import com.yatochk.pillapp.model.db.MedicationEatTypeConverter
 import com.yatochk.pillapp.model.db.MedicationTypeConverter
 import java.util.*
 
@@ -11,14 +12,17 @@ import java.util.*
 @TypeConverters(
     MedicationTypeConverter::class,
     ListTypeConverter::class,
-    DateTypeConverter::class
+    DateTypeConverter::class,
+    MedicationEatTypeConverter::class
 )
 data class MedicationSchedule(
     @PrimaryKey(autoGenerate = true)
     val id: Int?,
     val name: String,
     val dosage: Int,
+    val countInDay: Int,
     val periods: Long,
+    val dependencyOfEat: MedicationEat,
     val duration: Long,
     val startDate: Date,
     val receptionTimes: List<Long>,
